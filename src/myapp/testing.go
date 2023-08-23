3a7d8ac6237271c662807b4e8ce9eacf0827eb5a
Antonius Silvianus <antoniussilvianus@outlook.com>
2023-08-23 20:38:59 +0700

    coba testing

--- src/myapp/testing.go
+++ src/myapp/testing.go
@@ -3,7 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	for num := 1; num <= 10; num++ {
-		fmt.Printf("i ke %d\n", num)
+	var nilai int
+	nilai = 90
+	switch nilai {
+	case 100:
+		fmt.Println("Bagus Banget!")
+		break
+	case 90:
+		fmt.Println("Bagus Aja")
+		break
+	case 80:
+		fmt.Println("Bagus Lah")
+		break
+	default:
+		fmt.Println("Belajar lagi Lah")
 	}
 }
