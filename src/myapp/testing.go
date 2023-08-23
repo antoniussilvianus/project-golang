2ad845764626258c851854ad28452f77ba4c91d0
Antonius Silvianus <antoniussilvianus@outlook.com>
2023-08-23 19:48:24 +0700

    last commit

--- src/myapp/testing.go
+++ src/myapp/testing.go
@@ -3,18 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	nilai := 90
-
-	switch nilai {
-	case 100:
-		fmt.Println("Mantep")
-	case 90:
-		fmt.Println("Sabi")
-	case 80:
-		fmt.Println("Ok Lah")
-	case 70:
-		fmt.Println("Belajar lah")
-	default:
-		fmt.Println("Belajar lagi")
+	for num := 1; num <= 10; num++ {
+		fmt.Printf("i ke %d\n", num)
 	}
 }
